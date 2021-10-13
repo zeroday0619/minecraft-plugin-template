2fe505109eb9ac3e32954d2696243953e8ef2a99
Euiseo Cha <zeroday0619_dev@outlook.com>
2021-10-13 14:55:27 +0900

    chore: support minecraft 1.17.x

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,43 +1,24 @@
-import org.gradle.kotlin.dsl.support.zipTo
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-plugins {
-    kotlin("jvm") version "1.4.21"
-    kotlin("plugin.serialization") version "1.4.21"
-    id("kr.entree.spigradle") version "2.2.3"
+plugins {              
+    kotlin("jvm") version "1.5.31"
 }
-
 
 group = "com.github.zeroday0619"
 version = "1.0-SNAPSHOT"
 
-repositories {
+repositorys {
     mavenCentral()
-    maven("https://maven.heartpattern.kr/repository/maven-public/")
     maven("https://papermc.io/repo/repository/maven-public/")
-    jcenter()
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation(kotlin("reflect"))
-    implementation("io.papermc:paperlib:1.0.6")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
-    compileOnly("com.destroystokyo.paper:paper-api:1.16.4-R0.1-SNAPSHOT")
+    implementation("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
 }
 
-val shade = configurations.create("shade")
-shade.extendsFrom(configurations.implementation.get())
-
 tasks {
-    jar {
-        from(
-            shade.map {
-                if (it.isDirectory)
-                    it
-                else
-                    zipTree(it)
-            }
-        )
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = JavaVersion.VERSION_16.toString()
     }
 }
